356d31fc03944e33adc1f7b7c500c558b08d171f
Jason Adesanya <jason@circumuk.com>
2026-08-11 23:25:35 +0100

    Gate Rider offer polling on healthy location

--- lib/app/home/bloc/home_bloc.dart
+++ lib/app/home/bloc/home_bloc.dart
@@ -216,7 +216,9 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
           _startPresenceHeartbeat();
           emit(state.copyWith(
               rideStatus: RideStatus.online, canGoOnline: true, message: null));
-          add(GetAvailableRequests());
+          if (locationPayload != null) {
+            add(GetAvailableRequests());
+          }
           add(SetDrawerHeight(
               minDrawerHeight: state.minDrawerHeight,
               maxDrawerHeight: 0.75.sh));
