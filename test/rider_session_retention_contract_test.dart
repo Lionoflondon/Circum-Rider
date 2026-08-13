1864a78ea6ddc1b1b16b9446a5006c655267a29f
Jason Adesanya <jason@circumuk.com>
2026-08-13 01:08:18 +0100

    fix(rider): retain restored sessions

---
+++ test/rider_session_retention_contract_test.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('restored Rider sessions are not expired by account creation date', () {
+    final authBloc =
+        File('lib/app/authentication/bloc/auth_bloc.dart').readAsStringSync();
+
+    expect(authBloc, isNot(contains("DateTime.parse('2024-05-15')")));
+    expect(
+      authBloc,
+      isNot(contains('Future.delayed(const Duration(seconds: 3))')),
+    );
+    expect(authBloc, contains('on<SignOut>'));
+    expect(authBloc, contains('await auth.signOut()'));
+  });
+}
