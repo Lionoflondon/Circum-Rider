54f4fe1961e131ba07376a24ad371121981ef586
Jason Adesanya <jason@circum.local>
2026-08-16 00:21:11 +0100

    fix: preserve rider session restore online access

--- lib/app/authentication/bloc/auth_bloc.dart
+++ lib/app/authentication/bloc/auth_bloc.dart
@@ -145,15 +145,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
               riderAccountState: riderAccountState,
               authenticatedStatus: authenticatedStatus));
 
-          await Future.delayed(const Duration(seconds: 3));
-
-          final creationDate = DateTime.parse('${user.metadata.creationTime}');
-
-          final authChangeDate = DateTime.parse('2024-05-15');
-
-          if (authChangeDate.isAfter(creationDate)) {
-            add(SignOut());
-          }
         } else {
           emit(state.copyWith(currentState: AppState.unauthenticated));
         }
