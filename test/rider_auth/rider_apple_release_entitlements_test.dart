501d2e22f4a8d7ca35d455705afeb38511b3a4cd
Jason Adesanya <jason@circumuk.com>
2026-09-08 00:06:16 +0100

    Add Rider Apple privacy manifest

--- test/rider_auth/rider_apple_release_entitlements_test.dart
+++ test/rider_auth/rider_apple_release_entitlements_test.dart
@@ -16,4 +16,15 @@ void main() {
     );
     expect(entitlements, isNot(contains('<string>development</string>')));
   });
+
+  test('Rider packages a privacy manifest without tracking', () {
+    final manifest =
+        File('ios/Runner/PrivacyInfo.xcprivacy').readAsStringSync();
+    final project =
+        File('ios/Runner.xcodeproj/project.pbxproj').readAsStringSync();
+
+    expect(manifest, contains('<key>NSPrivacyTracking</key>\n\t<false/>'));
+    expect(manifest, contains('NSPrivacyAccessedAPITypes'));
+    expect(project, contains('PrivacyInfo.xcprivacy in Resources'));
+  });
 }
