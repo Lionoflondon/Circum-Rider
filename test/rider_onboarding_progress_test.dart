19fd02092ec4c3018a9e1c32e93b785adc44cef2
Jason Adesanya <jason@circumuk.com>
2026-09-21 20:56:57 +0100

    Keep onboarding contract formatting stable

--- test/rider_onboarding_progress_test.dart
+++ test/rider_onboarding_progress_test.dart
@@ -136,11 +136,9 @@ void main() {
     final source =
         File('lib/app/authentication/bloc/auth_bloc.dart').readAsStringSync();
     expect(source, contains("httpsCallable('verifyRiderAccountAccess')"));
-    expect(
-        source
-            .split('await verifyRiderSurfaceAfterAuthentication(')
-            .length,
-        5);
+    final guardedAuthCalls =
+        source.split('await verifyRiderSurfaceAfterAuthentication(').length;
+    expect(guardedAuthCalls, 5);
     expect(source, contains('await verifyRiderSurface(user);'));
     final guard = source.substring(
         source.indexOf('Future<void> verifyRiderSurface('),
